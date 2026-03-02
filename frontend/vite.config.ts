c62993b8fdee68103b1f082f6705220f0e13132e
Stuart Lynn <stuart.lynn@gmail.com>
2026-03-02 13:09:47 +0000

    Switching to 127.0.0.1 for oauth redirect

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	server: {
+		host: '127.0.0.1',
+		port: 5173,
 		proxy: {
 			'/api': {
 				target: 'http://localhost:3000',
